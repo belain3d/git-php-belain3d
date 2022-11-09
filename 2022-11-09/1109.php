023bba51303048f725086e6374cec1072a4d8186
belain3d <belain3d@gmail.com>
2022-11-09 15:03:26 +0100

    2022-11-09 added

---
+++ 2022-11-09/1109.php
@@ -0,0 +1,23 @@
+<?php
+
+$v = 3;
+
+switch ($v) {
+    case 1:
+        echo "Egy";
+        break;
+
+    case 2:
+        echo "Kettő";
+        break;
+
+    case 3:
+        echo "Három";
+        break;
+
+    default:
+        echo "A változó értéke nem 1, 2 vagy 3";
+
+}
+
+?>
